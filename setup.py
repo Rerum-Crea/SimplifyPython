af7e27d258dcaa68d82b668647ce7ce445e14c07
Necronwyx <necrownyx@outlook.com>
2022-04-09 17:42:14 +1000

    File Setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Simplify-Python",
+    name="SimplifyPython",
     version="0.0.1",
     author="Isaac",
     author_email="necrownyx@outlook.com",
