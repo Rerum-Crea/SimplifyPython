609c13cf5a169975493d9fcf83fcfc69babdbde2
Isaac <Necrownyx@outlook.com>
2022-04-11 11:52:20 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="SimplifyPython",
-    version="0.0.6",
+    version="0.0.7",
     author="Isaac",
     author_email="necrownyx@outlook.com",
     description="Simplifys aspects of python",
