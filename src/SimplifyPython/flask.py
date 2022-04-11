78f6639ad2edd3cbb90d6ec8d4873037f69f79e4
Restyled.io <commits@restyled.io>
2022-04-11 01:33:34 +0000

    Restyled by black

--- src/SimplifyPython/flask.py
+++ src/SimplifyPython/flask.py
@@ -1,2 +1,10 @@
 class sflask:
-    from PingServer import bcolors, create_page, launch_pages, launch_pages_nothread, start, thread, help
+    from PingServer import (
+        bcolors,
+        create_page,
+        launch_pages,
+        launch_pages_nothread,
+        start,
+        thread,
+        help,
+    )
--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from time import time
 from SimplifyPython import sflask
 
 var = 10
-sflask.create_page('/', 'hello, world!')
-sflask.create_page('/no', 'goodbye')
+sflask.create_page("/", "hello, world!")
+sflask.create_page("/no", "goodbye")
 sflask.launch_pages().start()
