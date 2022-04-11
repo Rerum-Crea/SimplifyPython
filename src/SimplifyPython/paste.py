655a775f17f772e77fc884d1f85ecbd6654c639b
Necronwyx <necrownyx@outlook.com>
2022-04-11 16:16:01 +1000

    Added simple read to spaste

--- src/SimplifyPython/paste.py
+++ src/SimplifyPython/paste.py
@@ -1,18 +1,26 @@
 class spaste:
 
     def text(content, url="https://www.toptal.com/developers/hastebin"):
-        link = internals(content, url)
+        link = write_internals(content, url)
         print(link)
         return link
 
     def file(filename, url="https://www.toptal.com/developers/hastebin"):
         data = stitch(filename)
-        link = internals(data, url)
+        link = write_internals(data, url)
         print(link)
         return link
 
+    def read(url):
+        from requests import get
+        data = get(url)
+        content = data.text
+        print(content)
+        return content
 
-def internals(content, url):
+
+
+def write_internals(content, url):
     from requests import post
 
     poster = post(f"{url}/documents", data=content.encode("utf-8"))
