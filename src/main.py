be94eb4065eb76718974a0185663c2ebd458f388
Necronwyx <necrownyx@outlook.com>
2022-04-12 15:08:49 +1000

    Changes

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 from fileinput import filename
 import requests
 import easygui
-from SimplifyPython import scrypto
+from SimplifyPython import sfile
 
-#def main():
-    # filepath = easygui.fileopenbox()
+def main():
+    filepath = easygui.fileopenbox()
     # sfile.send_encrypted('https://Messin.necrownyx.repl.co', filepath, b'qUVKJy_nN8H8TUSkhbC2X7WjfX7jm51BhD_KbjUwkZw=')
-    # sfile.send('https://Messin.necrownyx.repl.co', filepath)
+    sfile.send('https://Messin.necrownyx.repl.co', filepath)
 
-# main()
-
-print(scrypto.generate_key('', b''))
+main()
