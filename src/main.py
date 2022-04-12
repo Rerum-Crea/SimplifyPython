9484174faa448a859e608be15f6e2f2fef53b51b
Necronwyx <necrownyx@outlook.com>
2022-04-12 13:48:38 +1000

    gg

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from SimplifyPython import sfile, spaste
 
 def main():
     filepath = easygui.fileopenbox()
-    # sfile.send_encrypted('https://Messin.necrownyx.repl.co', filepath, b'qUVKJy_nN8H8TUSkhbC2X7WjfX7jm51BhD_KbjUwkZw=')
-    sfile.send('https://Messin.necrownyx.repl.co', filepath, '123')
+    sfile.send_encrypted('https://Messin.necrownyx.repl.co', filepath, b'qUVKJy_nN8H8TUSkhbC2X7WjfX7jm51BhD_KbjUwkZw=')
 
 main()
